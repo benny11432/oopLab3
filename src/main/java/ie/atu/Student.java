37e3feb03f79ccb9b51259610c5dd12e0d4d5383
benryan <g00462517@atu.ie>
2025-10-15 18:14:02 +0100

    Added classes, within student class i added strings, and a constructor

---
+++ src/main/java/ie/atu/Student.java
@@ -0,0 +1,15 @@
+package ie.atu;
+
+public class Student {
+    //data fields
+    private String name;
+    private String email;
+    private String course;
+
+    //constructor - to run when i create a new student
+    public Student (String name, String, email, String course) {
+        this.name = name;
+        this.email = email;
+        this.course = course;
+    }
+}
